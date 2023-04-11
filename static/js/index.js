1259b6885f010ad81af109376029b9beccd47522
Guan-rui Xu <100293836+hsumihuang@users.noreply.github.com>
2023-04-11 20:03:15 +0800

    Add files via upload

---
+++ static/js/index.js
@@ -0,0 +1,11 @@
+
+// Open and close sidebar
+function openNav() {
+              document.getElementById("mySidebar").style.width = "250px";
+              document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
